15f889a585dc263091ad81878b0482a29eef55cd
mirzahm14 <mirzahm14@gmail.com>
2024-03-03 23:21:21 +0700

    init project

---
+++ webapi/Program.cs
@@ -0,0 +1,34 @@
+using Microsoft.EntityFrameworkCore;
+using webapi.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+//load .env file
+DotNetEnv.Env.Load();
+
+// Add services to the container.
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString(Environment.GetEnvironmentVariable("DefaultConnection")));
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
